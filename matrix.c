a064753433c46b6e66912457c5233244e839dfaf
HaLoc1111 <haducloc1910vl@gmail.com>
2025-03-23 22:55:17 +0700

    khai bao du lieu ma tran

--- matrix.c
+++ matrix.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <math.h>
+#include "stdlib.h"
+#define M 100
+#define N 100
 int main( ) {
-    //
+    void duLieu (int Matrix[][M],int nRow, int nCol, char*nameMatrix) ;
+    //khai bao cac bien nhap vao du lieu
 
 
     return 0;
