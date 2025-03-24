c92688ddc0639d59d8df12bd8a9f75b417dfbd9b
HaLoc1111 <haducloc1910vl@gmail.com>
2025-03-24 09:37:39 +0700

    chua biet

--- matrix.c
+++ matrix.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-#include "stdlib.h"
+#include <stdlib.h>
 #define M 100
 #define N 100
 int main( ) {
     void duLieu (int Matrix[][M],int nRow, int nCol, char*nameMatrix) ;
     //khai bao cac bien nhap vao du lieu
+    int ** Add2Matrix 
+    void 
 
 
     return 0;
